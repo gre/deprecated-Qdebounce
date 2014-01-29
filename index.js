112b3061c3fa5da323669bef34527b15714312f8
Gaëtan Renaudeau <gre@zenexity.com>
2014-01-29 16:08:34 +0100

    0.1.0: using isPending()

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var Q = require("q");
 
-module.exports = function Qdebounce (func, wait) {
+function Qdebounce (func, wait) {
     var promise = Q();
     var context, args;
     return function () {
         context = this;
         args = arguments;
-        if (!promise.isFulfilled()) return promise;
-        promise = Q.delay(wait)
+        if (!promise.isPending()) {
+            promise = Q.delay(wait)
             .then(function () {
                 return Q.fapply(_.bind(func, context), args);
             });
+        }
         return promise;
     };
-};
+}
+
+module.exports = Qdebounce;
