19f690301762249db69d9aaeb3e3a44b41e79a5e
Gaëtan Renaudeau <gre@zenexity.com>
2014-02-04 14:43:08 +0100

    If a call happened during the func Promise, a new func call is scheduled

--- index.js
+++ index.js
@@ -4,14 +4,34 @@ var _ = require("lodash");
 function Qdebounce (func, wait) {
     var promise = Q();
     var context, args;
+    var recallNeeded = false;
     return function () {
         context = this;
         args = arguments;
         if (!promise.isPending()) {
             promise = Q.delay(wait)
-            .then(function () {
-                return Q.fapply(_.bind(func, context), args);
+            .then(function rec () {
+                recallNeeded = false;
+                return Q.fapply(_.bind(func, context), args)
+                    .then(function (res) {
+                        if (recallNeeded) {
+                            return rec();
+                        }
+                        else {
+                            return res;
+                        }
+                    }, function (e) {
+                        if (recallNeeded) {
+                            return rec();
+                        }
+                        else {
+                            throw e;
+                        }
+                    });
             });
+        }
+        else {
+            recallNeeded = true;
         }
         return promise;
     };
